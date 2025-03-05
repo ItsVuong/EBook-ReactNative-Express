d9e7ae013bdb3fcc5fb674b54a1bda26c5a51ad0
ItsVuong <itsvuong0806@gmail.com>
2025-03-05 10:38:37 +0700

    Update

--- client/app/(tabs)/Home.tsx
+++ client/app/(tabs)/Home.tsx
@@ -1,11 +1,42 @@
 import ParallaxScrollView from "@/components/ParallaxScrollView";
-import { View } from "react-native";
+import { StyleSheet, TouchableOpacity, View, Text, TextInput } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function HomeScreen(){
-  return(
-    <SafeAreaView style={{flex: 1}}>
-      <View>Home Screen</View>
+function searchBar() {
+  return (
+    <View style={{
+      flex: 1, flexDirection: 'row', paddingHorizontal: 8,
+      alignItems: 'center'
+    }}>
+      <TextInput style={{ borderWidth: 0.5, width: '100%', padding: 10, borderRadius: 20}}
+        placeholder="Search"
+      />
+    </View>
+  )
+}
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ height: 60 }}>
+        {searchBar()}
+      </View>
+        <View style={styles.fake_post}></View>
+        <View style={styles.fake_post}></View>
+        <View style={styles.fake_post}></View>
+        <View style={styles.fake_post}></View>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  image: {
+    width: 100, height: 150, borderRadius: 10
+  },
+  fake_post: {
+    backgroundColor: '#e4e6eb',
+    height: 200,
+    margin: 16,
+    borderRadius: 16
+  }
+});
